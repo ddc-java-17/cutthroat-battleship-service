443db581dfa6260232c0b07c5e3c5db391cb06d1
reedsearle <reed_searle@hotmail.com>
2024-02-21 13:37:41 -0700

    Initial project creation

---
+++ src/main/java/edu/cnm/deepdive/cutthroatbattleshipservice/CutthroatBattleshipServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.cutthroatbattleshipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutthroatBattleshipServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CutthroatBattleshipServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/cutthroatbattleshipservice/CutthroatBattleshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.cutthroatbattleshipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutthroatBattleshipServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
