94b72cf66cf47464747c71727dad5cf38ccd9349
DanielleScott <dmyers28@cnm.edu>
2024-03-01 17:51:40 -0700

    Added a field for the user. Added a field for user_game_id.

--- src/main/java/edu/cnm/deepdive/cutthroatbattleshipservice/model/entity/UserGame.java
+++ src/main/java/edu/cnm/deepdive/cutthroatbattleshipservice/model/entity/UserGame.java
@@ -3,29 +3,36 @@ package edu.cnm.deepdive.cutthroatbattleshipservice.model.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import org.springframework.lang.NonNull;
 
 @Entity
-@Table()
+@Table(name = "user_id")
 @JsonInclude(value = Include.NON_NULL)
 @JsonPropertyOrder({})
 public class UserGame {
 
-
-
-  @Id
   @NonNull
+  @Id
   @GeneratedValue
-  @Column(name = "user_profile_id", nullable = false, updatable = false)
+  @Column(name = "user_game_id", nullable = false, updatable = false)
   @JsonIgnore
   private Long id;
 
-
+  @NonNull
+  @ManyToOne(optional = false, fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  @JsonProperty(access = Access.READ_ONLY)
+  private User user;
 
 }
