0516df9772a8c9a7ddcae40bf914f291b27f9fe7
DanielleScott <dmyers28@cnm.edu>
2024-03-21 14:59:33 -0600

    Removed references to Fleet in imports

--- src/main/java/edu/cnm/deepdive/jata/model/dao/ShipRepository.java
+++ src/main/java/edu/cnm/deepdive/jata/model/dao/ShipRepository.java
@@ -1,8 +1,6 @@
 package edu.cnm.deepdive.jata.model.dao;
 
-import edu.cnm.deepdive.jata.model.entity.Fleet;
 import edu.cnm.deepdive.jata.model.entity.ShipLocation;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShipRepository extends JpaRepository<ShipLocation, Long> {
